f902e9b484be9b3d49e471d7176b40527f9c4e13
seirra blake <sophietheopossum@yandex.ru>
2023-04-18 12:25:49 +0100

    add system set command

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 use std::fs;
+use std::process::Command;
 
 fn main() {
     let contents = fs::read_to_string("/home/seirra/Documents/src/hdr/.gitignore")
         .expect("file read error");
+    let system_set = Command::new("sh")
+            .arg("-c")
+            .arg("eix -c --system")
+            .output()
+            .expect("failed to execute process");
+    println!("status: {}", system_set.status);
+    println!("stdout: {}", String::from_utf8_lossy(&system_set.stdout));
+    println!("stderr: {}", String::from_utf8_lossy(&system_set.stderr));
     println!("{contents}");
 }
