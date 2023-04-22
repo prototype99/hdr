5bfb809e29d9d15b9af670418ce66e93d2b3b1f3
seirra blake <sophietheopossum@yandex.ru>
2023-04-22 19:14:09 +0100

    use match statement and add more tests

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,37 @@
-use std::{env, fs};
+use std::{fs};
+use std::env::args;
 use std::process::Command;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        panic!("no commands specified, either choose install, or update");
-    } else {
-        let mode = &args[1];
-        if mode == "update" {
-            println!("updating system...");
-            let contents = fs::read_to_string("/etc/portage/make.conf")
-                .expect("file read error");
-            let system_set = Command::new("sh")
-                .arg("-c")
-                .arg("eix-update && eix -c --system")
-                .output()
-                .expect("failed to execute process");
-            println!("status: {}", system_set.status);
-            println!("stdout: {}", String::from_utf8_lossy(&system_set.stdout));
-            println!("stderr: {}", String::from_utf8_lossy(&system_set.stderr));
-            println!("{contents}");
-        } else if mode == "install" {
-            let modi = &args[2];
-            println!("Installing {}...", modi);
+    let args: Vec<String> = args().collect();
+    match args.len() {
+        1 =>  panic!("no commands specified, please choose install, or update"),
+        2 => {
+            if &args[1] == "update" {
+                println!("updating system...");
+                let contents = fs::read_to_string("/etc/portage/make.conf")
+                    .expect("file read error");
+                let system_set = Command::new("sh")
+                    .arg("-c")
+                    .arg("eix-update && eix -c --system")
+                    .output()
+                    .expect("failed to execute process");
+                println!("status: {}", system_set.status);
+                println!("stdout: {}", String::from_utf8_lossy(&system_set.stdout));
+                println!("stderr: {}", String::from_utf8_lossy(&system_set.stderr));
+                println!("{contents}")
+            } else if &args[1] == "install" {
+                panic!("please specify packages to install")
+            } else {
+                panic!("unknown command, please choose install, or update")
+            }
+        },
+        _ => {
+            if &args[1] == "install" {
+                println!("Installing {}...", &args[2]);
+            } else {
+                panic!("unknown command, please choose install, or update")
+            }
         }
     }
 }
