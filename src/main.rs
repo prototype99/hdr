c90fc3a2275a7ba078d5e1d58534cf09e0a42042
seirra blake <sophietheopossum@yandex.ru>
2023-04-22 16:40:22 +0100

    try checking for existing args

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,26 @@ use std::process::Command;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let mode = &args[1];
-    if mode == "update" {
-        println!("updating system...");
-        let contents = fs::read_to_string("/etc/portage/make.conf")
-            .expect("file read error");
-        let system_set = Command::new("sh")
-            .arg("-c")
-            .arg("eix-update && eix -c --system")
-            .output()
-            .expect("failed to execute process");
-        println!("status: {}", system_set.status);
-        println!("stdout: {}", String::from_utf8_lossy(&system_set.stdout));
-        println!("stderr: {}", String::from_utf8_lossy(&system_set.stderr));
-        println!("{contents}");
-    } else if mode == "install" {
-        let modi = &args[2];
-        println!("Installing {}...", modi);
+    if args.len() < 1 {
+        panic!("no commands specified, either choose install, or update");
+    } else {
+        let mode = &args[1];
+        if mode == "update" {
+            println!("updating system...");
+            let contents = fs::read_to_string("/etc/portage/make.conf")
+                .expect("file read error");
+            let system_set = Command::new("sh")
+                .arg("-c")
+                .arg("eix-update && eix -c --system")
+                .output()
+                .expect("failed to execute process");
+            println!("status: {}", system_set.status);
+            println!("stdout: {}", String::from_utf8_lossy(&system_set.stdout));
+            println!("stderr: {}", String::from_utf8_lossy(&system_set.stderr));
+            println!("{contents}");
+        } else if mode == "install" {
+            let modi = &args[2];
+            println!("Installing {}...", modi);
+        }
     }
 }
