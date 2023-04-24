e50c759edddf1b7a1281d86f1e5a200ffe790c1f
seirra blake <sophietheopossum@yandex.ru>
2023-04-24 09:08:15 +0100

    comment unneeded code

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,17 @@ fn main() {
         2 => {
             if &args[1] == "update" {
                 println!("updating system...");
-                let contents = fs::read_to_string("/etc/portage/make.conf")
-                    .expect("file read error");
+                //let contents = fs::read_to_string("/etc/portage/make.conf")
+                //    .expect("file read error");
                 let system_set = Command::new("sh")
                     .arg("-c")
                     .arg("readlink -f /etc/portage/make.profile")
                     .output()
                     .expect("failed to execute process");
-                println!("status: {}", system_set.status);
+                //println!("status: {}", system_set.status);
                 println!("stdout: {}", String::from_utf8_lossy(&system_set.stdout));
-                println!("stderr: {}", String::from_utf8_lossy(&system_set.stderr));
-                println!("{contents}")
+                //println!("stderr: {}", String::from_utf8_lossy(&system_set.stderr));
+                //println!("{contents}")
             } else if &args[1] == "install" {
                 panic!("please specify packages to install")
             } else {
