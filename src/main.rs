838599c730ae266906348d8875b2ad554e1d0010
seirra blake <sophietheopossum@yandex.ru>
2023-05-05 01:52:57 +0100

    partially fix error

--- src/main.rs
+++ src/main.rs
@@ -144,9 +144,10 @@ fn update() {
                 let file = File::open(path_real).unwrap();
                 for line in BufReader::new(file).lines() {
                     let unline = line.unwrap();
+                    let unline_end = unline.len()-1;
                     for use_expand in use_expands.clone() {
                         if unline.starts_with(use_expand) {
-                            let flag_split: Vec<&str> = unline.get(use_expand.len()+2..unline.len()-1).unwrap().split_whitespace().collect();
+                            let flag_split: Vec<&str> = unline.get(use_expand.len()+2..unline_end).unwrap().split_whitespace().collect();
                             for split in flag_split {
                                 use_flags.push(split);
                             }
